45093d746f1148b9e8ff8881fdebc09f95a82be6
Parth0124 <abhangparth@gmail.com>
2025-02-05 03:49:45 +0530

    commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_community.utilities import ArxivAPIWrapper, WikipediaAPIWrapper
+from langchain_community.tools import ArxivQueryRun, WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain.agents import initialize_agent, AgentType
+from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
+import os
+from dotenv import load_dotenv
+
+api_wrapper_wiki = WikipediaAPIWrapper(top_k_results = 1, doc_content_chars_max=250)
+wiki=WikipediaQueryRun(api_wrapper=api_wrapper_wiki)
+
+api_wrapper_arxiv = ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=300)
+arxiv = ArxivQueryRun(api_wrapper=api_wrapper_arxiv)
+
+search = DuckDuckGoSearchRun(name="Search")
+
+st.title("Langchain Chat With Search")
+
+st.sidebar.title("Settings")
+api_key = st.sidebar.text_input("Enter your Groq API key:", type="password")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"]=[
+        {"role":"assistant", "content":"Hi, I am a chatbot who can search the web. How can I help you?"}
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+##Prompt
+
+if prompt:= st.chat_input(placeholder="What is Machine Learning?"):
+    st.session_state.messages.append({"role":"user", "content":prompt})
+    st.chat_message("user").write(prompt)
+
+    llm = ChatGroq(api_key=api_key, model_name="Llama3-8b-8192", streaming=True)
+    tools = [search, arxiv,wiki]
+
+    #convert the tools to agent
+    search_agent=initialize_agent(tools,llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handling_parsing_error=True)
+
+    with st.chat_message("assistant"):
+        st_cb=StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
+        st.session_state.messages.append({"role":"assistant", "content":response})
+        st.write(response)
